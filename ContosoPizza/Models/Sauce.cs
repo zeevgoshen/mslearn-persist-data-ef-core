67efe7f03584921e7e9fee09cbe36ec1896e6bbe
Zeev <zeevgoshen@gmail.com>
2022-07-04 15:47:17 +0300

    update to model classes created a join table

--- ContosoPizza/Models/Sauce.cs
+++ ContosoPizza/Models/Sauce.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoPizza.Models;
 
 public class Sauce
 {
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(100)]
     public string? Name { get; set; }
 }
