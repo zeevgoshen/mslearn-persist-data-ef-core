f4995fa3c337d5017714842bb4d2673f2d4ce8eb
Zeev <zeevgoshen@gmail.com>
2022-07-04 15:30:08 +0300

    PizzaContext and ef migrations

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,4 +1,5 @@
 using ContosoPizza.Services;
+using ContosoPizza.Data;
 // Additional using declarations
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add the PizzaContext
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 
 // Add the PromotionsContext
 
